a2b87c6c809a8bde8134293385d6288a5b64ed4b
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-05-30 23:27:38 +0600

    sojdg

--- SpaceBattle/SpaceBattle.Lib/Class1.cs
+++ SpaceBattle/SpaceBattle.Lib/Class1.cs
@@ -1,5 +1,18 @@
 ﻿namespace SpaceBattle.Lib;
 
-public class Class1
+public class Summa
 {
+    private int a;
+    private int b;
+
+    public Summa(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int Sum()
+    {
+        return this.a + this.b;
+    }
 }
